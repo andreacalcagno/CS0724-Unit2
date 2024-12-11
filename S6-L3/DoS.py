1a53caedeac3a5bd86cf4a75c40ba2b99a07a405
andreacalcagno <calca2005@gmail.com>
2024-12-11 11:13:42 -0500

    correzoni

--- S6-L3/DoS.py
+++ S6-L3/DoS.py
@@ -1,10 +1,9 @@
 import socket
 import os
-import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 target = input("\nInserisci TARGET: ")
-porta = int(input("\nInerisci il numero della porta su cui vuoi ascoltare: "))
+porta = int(input("\nInserisci il numero della porta: "))
 quantità = int(input("\nQuanti pacchetti vuoi inviare? "))
     
 dimensione_pacchetto = 1024
